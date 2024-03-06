a7832f9a6c51a79dd934b138e5f0da01dbd09ea7
Garron-Haley <161738884+Garron-Haley@users.noreply.github.com>
2024-03-06 14:49:42 -0700

    Add files via upload

---
+++ Book/src/book.java
@@ -0,0 +1,110 @@
+
+public class book {
+
+	public static void main(String[] args) {
+		MyInteger user = new MyInteger("sixty");
+		System.out.println(user.isEven());
+		System.out.println(user.isOdd());
+		System.out.println(user.isPrime());
+		System.out.println(MyInteger.isEven(user));
+		System.out.println(MyInteger.isOdd(user));
+		System.out.println(MyInteger.isPrime(user));
+		MyInteger.value = 19;
+		System.out.println(MyInteger.isEven(user));
+		System.out.println(MyInteger.isOdd(user));
+		System.out.println(MyInteger.isPrime(user));
+		
+	}
+
+}
+class MyInteger {
+	static int value;
+	MyInteger(){
+		value = 1;
+	}
+	MyInteger(int newvalue) {
+		value = newvalue;
+	}
+	MyInteger(String a) {
+		value = Integer.parseInt(a);
+	}
+	MyInteger(char[] b){
+		value = Integer.parseInt(String.valueOf(b));
+	}
+	boolean isEven() {
+		return isEven(value); 
+	}
+	boolean isOdd() {
+		return isOdd(value);
+	}
+	boolean isPrime() {
+     return isPrime(value);
+    }
+	static boolean isEven(int value2) {
+		if (value2 % 2 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	static boolean isOdd(int value2) {
+		if (value % 2 == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	static boolean isPrime(int value2) {
+        if(value<=1)
+        {
+            return false;
+        }
+       for(int i=2;i<=value/2;i++)
+       {
+           if((value%i)==0)
+               return  false;
+       }
+       return true;
+   }
+	static boolean isEven(MyInteger value3) {
+		return isEven(value);
+	}
+	static boolean isOdd(MyInteger value3) {
+		return isOdd(value);
+	}
+	static boolean isPrime(MyInteger value3) {
+     return isPrime(value);
+        }
+     boolean equals(int value2) {
+    	 if (isEven(value) == isEven(value2)) {
+    		 return true;
+    	 }
+    	 else {
+    		 return false;
+    	 }
+     }
+     boolean equals(MyInteger value2) {
+    	 if (isEven(value) == isEven(value2)) {
+    		 return true;
+    	 }
+    	 else {
+    		 return false;
+    	 }
+     }
+
+	void parseInt(char[] b) {
+		value = Integer.parseInt(String.valueOf(b));
+		
+	}
+	void parseInt(String a) {
+		value = Integer.parseInt(a);
+		
+	}
+	void setValue(int newValue, String a, String b){
+		value = newValue;
+		value = Integer.parseInt(a);
+		value = Integer.parseInt(String.valueOf(b));
+	}
+}
